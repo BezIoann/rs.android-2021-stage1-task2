9de697b7f601fff8891497be8d95f621d1348ecd
Ioann <42849417+BezIoann@users.noreply.github.com>
2021-06-21 23:44:52 +0300

    init: finished third task

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,29 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when(blockB) {
+            // взависимости от класса blockB, работаем с массивом согласно условию
+            String::class -> {
+                //конкатенация строк
+                return blockA.filterIsInstance<String>().joinToString()
+            }
+            Int::class -> {
+                //сумма чисел
+                return blockA.filterIsInstance<Int>().sum()
+            }
+            LocalDate::class -> {
+                val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                // ближайшая к текущему дню дата
+                return blockA.filterIsInstance<LocalDate>().max()?.format(formatter)!!
+            }
+        }
+
+        return "Empty"
     }
 }
