d92e239d7d38234cc665ea822ebfdb98534b24d3
Ioann <42849417+BezIoann@users.noreply.github.com>
2021-06-21 23:44:08 +0300

    init: finished second task

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,17 @@
 package subtask2
 
 class Abbreviation {
-
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        // приведем строки к спискам
+        val firstList = a.toLowerCase().toList()
+        val secondList = b.toLowerCase().toList()
+        // с помощью intersect найдем пересечение двух списков и
+        // если пересечение есть и равно secondlist,
+        // то всё верно и возвращаем YES иначе NO
+        if (firstList.intersect(secondList).toList() == secondList) {
+            return "YES"
+        } else {
+            return "NO"
+        }
     }
 }
